5da98e0dab25e0f192ba157eecdc56b92b63593a
Ishan Ardithya Bandarigoda <55695411+IshanArdithya@users.noreply.github.com>
2024-10-15 15:43:16 +0530

    Update remove_cart.php

--- ajax/remove_cart.php
+++ ajax/remove_cart.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['remove_from_cart'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['product_id'])) {
     $product_id = intval($_POST['product_id']);
 
     if (isset($_SESSION['Cart'][$product_id])) {
@@ -11,10 +11,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['remove_from_cart'])) 
             unset($_SESSION['Cart'][$product_id]);
         }
 
-        echo json_encode(['success' => true]);
+        echo json_encode([
+            'success' => true,
+            'message' => 'Product removed from cart successfully.'
+            // 'itemCount' => array_sum($_SESSION['Cart']),
+        ]);
     } else {
-        echo json_encode(['success' => false]);
+        echo json_encode([
+            'success' => false,
+            'message' => 'Product not found in cart.',
+        ]);
     }
+    exit;
+} else {
+    echo json_encode(['success' => false, 'message' => 'Invalid request.']);
 }
-
 ?>
